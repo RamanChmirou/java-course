057e91b0932e98516ffaa454bfe23ba3226fa3dc
Roman Chmirov <chmirov2006@gmail.com>
2026-03-30 14:41:00 +0200

    +synchronize(with lock and condition)

--- synchronize/src/MyQueue.java
+++ synchronize/src/MyQueue.java
@@ -1,34 +1,56 @@
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class MyQueue {
     private final List<Element> elements = new LinkedList<>();
     private volatile boolean isFinished;
 
-    public synchronized void produce(Element element) throws InterruptedException {
-        while (elements.size() >= 5) {
-            wait();
+    private final Lock lock = new ReentrantLock();
+    private final Condition notFull = lock.newCondition();
+    private final Condition notEmpty = lock.newCondition();
+
+    public void produce(Element element) throws InterruptedException {
+        lock.lock();
+        try {
+            while (elements.size() >= 5) {
+                notFull.await();
+            }
+            elements.add(element);
+            System.out.printf("Producent dodał %s.%n", element);
+            notEmpty.signal();
+        } finally {
+            lock.unlock();
         }
-        elements.add(element);
-        System.out.printf("Producent dodał %s.%n", element);
-        notifyAll();
     }
 
-    public synchronized boolean consume() throws InterruptedException {
-        while (elements.isEmpty()) {
-            if (isFinished) {
-                return false;
+    public boolean consume() throws InterruptedException {
+        lock.lock();
+        try {
+            while (elements.isEmpty()) {
+                if (isFinished) {
+                    return false;
+                }
+                notEmpty.await();
             }
-            wait();
+            Element element = elements.removeFirst();
+            System.out.printf("Consument pobrał %s.%n", element);
+            notFull.signalAll();
+            return true;
+        } finally {
+            lock.unlock();
         }
-        Element element = elements.removeFirst();
-        System.out.printf("Consument pobrał %s.%n", element);
-        notifyAll();
-        return true;
     }
 
     public synchronized void setFinished(boolean finished) {
-        isFinished = finished;
-        notifyAll();
+        lock.lock();
+        try {
+            isFinished = finished;
+            notEmpty.signalAll();
+        } finally {
+            lock.unlock();
+        }
     }
 }
